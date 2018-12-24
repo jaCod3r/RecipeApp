9886732e05fe0ae7421fca2bd1d6bafce7740c32
Juber Ahmed <a.juber@hotmail.com>
2018-12-23 19:49:37 -0500

    Introduced ShoppingListService to existing code base

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,28 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Pizza', 'cheese', 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg'),
-    new Recipe('Pizza', 'cheese', 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg')
+    new Recipe(
+      'Pizza',
+      'cheese',
+      'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg',
+      [
+        new Ingredient('Cheese', 1),
+        new Ingredient('Tomato', 5)
+      ]),
+    new Recipe(
+      'Pizza',
+      'cheese',
+      'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg',
+      [
+        new Ingredient('Cheese', 1),
+        new Ingredient('Tomato', 5)
+      ])
   ];
 
   getRecipes() {
